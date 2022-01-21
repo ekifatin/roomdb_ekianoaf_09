bcced239d1743f35492d4ab47921fc355ff262c1
ekifatin <ekifatin4@gmail.com>
2022-01-21 10:14:16 +0700

    updated

---
+++ app/src/main/java/com/example/roomdb_ekianoaf_09/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.roomdb_ekianoaf_09
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        splashpic.alpha= 0f
+        splashpic.animate().setDuration(3000).alpha(1f).withEndAction{
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
